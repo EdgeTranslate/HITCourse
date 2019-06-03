bb21ff80400b93815d8899b9cb4e15fb2f6dfb54
nickyc975 <chenjinlong2016@gmail.com>
2019-06-04 02:11:09 +0800

    Revert "add operation response"
    This reverts commit 2231d099f0a0528e55fb71738aeeabb0cd49e780.

--- src/contents/content.js
+++ src/contents/content.js
@@ -6,8 +6,6 @@ var intervalId = null;
 var baseFormData = null;
 var courseFrame = document.getElementById("iframename");
 var queryForm = courseFrame.contentWindow.document.getElementById("queryform");
-var refreshButton = document.getElementById("person_info2");
-var coursesList = [];
 
 function getToken(callback) {
     var formData = new FormData();
@@ -98,32 +96,22 @@ courseFrame.onload = function() {
     queryForm = courseFrame.contentWindow.document.getElementById("queryform");
     var courseTable = getTable();
     if (courseTable) {
-        var status = coursesList.length === 0; // indicate if query has started  true:not start false: started
         var submit = document.createElement("button");
-        if (status) {
-            submit.innerText = "开始抢课";
-        } else {
-            submit.innerText = "结束选课";
-        }
+        submit.innerText = "开始抢课";
 
         courseTable.childNodes[0].childNodes[1].appendChild(submit);
         submit.onclick = function() {
-            var status = coursesList.length === 0; // indicate if query has started  true:not start false: started
+            var content = [];
+            // var courseTable = getTable();
+            // if (courseTable) {
             for (var i = 1; i < courseTable.children.length; i++) {
                 var tempDiv = courseTable.childNodes[2 * i].childNodes[1].childNodes[1];
-                if (status) {
-                    if (tempDiv.childNodes[0].checked) {
-                        coursesList.push(tempDiv.childNodes[2].id);
-                    }
+                if (tempDiv.childNodes[0].checked) {
+                    content.push(tempDiv.childNodes[2].id);
                 }
             }
-            if (status) {
-                start(coursesList);
-            } else {
-                stop();
-                coursesList = [];
-            }
-            refreshButton.click();
+            start(content);
+            // }
         };
 
         for (var i = 1; i < courseTable.children.length; i++) {
@@ -138,11 +126,6 @@ courseFrame.onload = function() {
             // button.style.cssFloat = "left";
             var tempDiv = courseTable.childNodes[2 * i].childNodes[1].childNodes[1];
             tempDiv.insertBefore(button, tempDiv.firstChild);
-            if (!status) {
-                if (coursesList.indexOf(tempDiv.childNodes[2].id) !== -1) {
-                    tempDiv.childNodes[0].checked = true;
-                }
-            }
         }
     }
 };
