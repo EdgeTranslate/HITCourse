42a16ca551cf37fcf4b632734890cef5216779f2
Mark <f18846188605@gmail.com>
2019-06-03 23:54:25 +0800

    fix one small problem

--- src/contents/content.js
+++ src/contents/content.js
@@ -70,27 +70,24 @@ function getTable() {
 }
 
 courseFrame.onload = function() {
+    queryForm = courseFrame.contentWindow.document.getElementById("queryform");
     var courseTable = getTable();
     if (courseTable) {
         var submit = document.createElement("button");
         submit.innerText = "开始抢课";
-        var message = {};
-        message.type = "start";
-        message.content = [];
+
         courseTable.childNodes[0].childNodes[1].appendChild(submit);
         submit.onclick = function() {
-            var message = {};
-            message.type = "start";
-            message.content = [];
+            var content = [];
             // var courseTable = getTable();
             // if (courseTable) {
             for (var i = 1; i < courseTable.children.length; i++) {
                 var tempDiv = courseTable.childNodes[2 * i].childNodes[1].childNodes[1];
                 if (tempDiv.childNodes[0].checked) {
-                    message.content.push(tempDiv.childNodes[2].id);
+                    content.push(tempDiv.childNodes[2].id);
                 }
             }
-            start(message);
+            start(content);
             // }
         };
 
