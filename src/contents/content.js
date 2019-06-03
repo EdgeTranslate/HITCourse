a51ea2b1f3fb399949d298b74d85509ff9546d85
nickyc975 <chenjinlong2016@gmail.com>
2019-06-03 23:01:32 +0800

    Finished select course functions.

--- src/contents/content.js
+++ src/contents/content.js
@@ -1,26 +1,56 @@
-window.oldAlert = window.alert;
-window.alert = function(msg) {
-    window.selectStatus = msg;
-};
+const ALERT_PATTERN = /alert\(['"](.*?)['"]\);/;
 
+var courses = null;
 var intervalId = null;
+var baseFormData = null;
 var courseFrame = document.getElementById("iframename");
-var refreshButton = document.getElementById("person_info2");
-var courseButton = courseFrame.contentWindow.document.getElementById("2018-2019-3-CS33903B-001");
+var queryForm = courseFrame.contentWindow.document.getElementById("queryform");
 
-function selectCourse() {
-    refreshButton.click();
-    courseButton.click();
-    if (window.selectStatus && window.selectStatus === "选课成功") {
-        window.oldAlert("选课成功");
+function watchCourses() {
+    var finished = true;
+    courses.forEach(course => {
+        if (!course.selected) {
+            selectCourse(course);
+        }
+        finished = finished & course.selected;
+    });
+
+    if (finished) {
         clearInterval(intervalId);
     }
 }
 
+function selectCourse(course) {
+    var formData = new FormData();
+    var request = new XMLHttpRequest();
+    baseFormData.forEach((value, key) => {
+        formData.append(key, value);
+    });
+
+    formData.set("rwh", course.courseNo);
+    request.open("POST", "/xsxk/saveXsxk");
+    request.send(formData);
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                var msg = ALERT_PATTERN.exec(request.responseText)[1];
+                if (msg.includes("选课成功")) {
+                    course.selected = true;
+                }
+            }
+        }
+    };
+}
+
 chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-    switch (message) {
+    switch (message.type) {
         case "start":
-            intervalId = setInterval(selectCourse, 3000);
+            courses = new Array();
+            message.content.forEach(course => {
+                courses.push({ courseNo: course, selected: false });
+            });
+            baseFormData = new FormData(queryForm);
+            intervalId = setInterval(watchCourses, 3000);
             break;
         case "stop":
             if (intervalId) {
